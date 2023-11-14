f289cf738aff7f31d1b9db10559f4f25c4e6dfba
omidNomiri <omidnomiri@gmail.com>
2023-11-14 21:46:52 +0330

    fix chess page.

--- Assignment_5/chess_page.py
+++ Assignment_5/chess_page.py
@@ -1,12 +1,14 @@
-import random
-
 def chess_page_output(row: int,column: int):
-    chess_list = [[0]*column for _ in range(row)]
     for i in range(row):
         for j in range(column):
-            chess_list[i][j]= random.randint(1,9)
 
-    print(chess_list)
+            if (i + j) % 2 == 0:
+                print("⬛", end='')
+
+            else:
+                print("⬜", end='')
+
+        print()
 
 chess_page_output(int(input("Please ener your row: ")),int(input("Please ener your column: ")))
 
