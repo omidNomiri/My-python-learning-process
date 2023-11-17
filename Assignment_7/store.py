c2a4722651213758d6cf0cf3f825677094be66fd
omidNomiri <omidnomiri@gmail.com>
2023-11-17 13:26:48 +0330

    FIX store (add def save in database,search,remove)

--- Assignment_7/store.py
+++ Assignment_7/store.py
@@ -1,11 +1,11 @@
-PRODUCT = []
+PRODUCTS = []
 
 def read_database():
     with open("Assignment_7\database.txt","r") as database:
         for line in database:
             product_list = line.split(",")
             my_dictionary = {"code" : product_list[0],"name" : product_list[1],"price" : product_list[2],"storage" : product_list[3]}
-            PRODUCT.append(my_dictionary)
+            PRODUCTS.append(my_dictionary)
 
 print("Loading.....")
 read_database()
@@ -25,36 +25,42 @@ def add():
     name = str(input("Please enter your commodity name: "))
     price = int(input("Please enter your commodity price: "))
     storage = int(input("Please enter your commodity storage: "))
-    PRODUCT.append({"code":code,"name":name,"price":price,"storage":storage})
+    new_product = {"code":code,"name":name,"price":price,"storage":storage}
+    PRODUCTS.append(new_product)
 
 def edit():
-    print("1.name")
-    print("2.price")
-    print("3.storage")
-    choice = input("You want edit which field: ")
-    if choice == 1:
-        change_value = input("Enter your commodity name: ")
-        PRODUCT.append(change_value[1])
-    elif choice == 2:
-        change_value = input("Enter your commodity price: ")
-        PRODUCT.append(change_value[2])
-    elif choice == 3:
-        change_value = input("Enter your commodity storage: ")
-        PRODUCT.append(change_value[3])
+    ...
 
 def remove():
-    ...
+    code = int(input("Please enter your product id: "))
+    PRODUCTS = [row for row in PRODUCTS if row['code'] != code]
+    print("Remove successful.")
 
 def search():
-    ...
+    user_search = input("Please enter your product id or name: ")
+    for product in PRODUCTS:
+        if product["code"]==user_search or product["name"]:
+            print(product["code"],"\t",product["name"],"\t",product["price"])
+            break
+    else:
+        print("not find.")
 
 def show_list():
-    for row in PRODUCT:
-        print(row)
+    for row in PRODUCTS:
+        print(row["code"],"\t",row["name"],"\t",row["price"])
 
 def buy():
     ...
 
+def save_in_database():
+    with open("Assignment_7\database.txt","a") as database:
+        for row in PRODUCTS:
+            code = row["code"]
+            name = row["name"]
+            price = row["price"]
+            storage = row["storage"]
+            data = str(f"{code},{name},{price},{storage}")
+            database.write(data)
 
 while True:
     show_menu()
@@ -72,7 +78,7 @@ while True:
     elif user_operation == 6:
         buy()
     elif user_operation == 7:
-        with open("Assignment_7\database.txt","w") as database:
-            for row in PRODUCT:
-                database.write(str({"code":row["code"],"name":row["name"],"price":row["price"],"storage":row["storage"]})+"\n")
+        save_in_database()
         exit(0)
+    else:
+        print("enter number between 1 to 7.")
