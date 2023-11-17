5d7cff3342d33e684eb07f9e27d552b69b089f15
omidNomiri <omidnomiri@gmail.com>
2023-11-17 22:50:45 +0330

    ADD gifmaker and translate (have a some problem..)

---
+++ Assignment_8/gif_maker.py
@@ -0,0 +1,12 @@
+import os
+import imageio
+
+file_list = sorted(os.listdir("Assignment_8/images"))
+
+IMAGES = []
+for file_name in file_list:
+    file_path = "Assignment_8/images/" + file_name
+    image = imageio.v2.imread(file_path)
+    IMAGES.append(image)
+
+imageio.mimsave("Assignment_8/drawing.gif", IMAGES)
