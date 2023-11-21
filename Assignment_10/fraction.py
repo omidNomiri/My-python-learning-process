46e08c6ec4fa7dc92fa96cd3c848915233990263
omidNomiri <omidnomiri@gmail.com>
2023-11-21 19:30:37 +0330

    ADD fraction file

---
+++ Assignment_10/fraction.py
@@ -0,0 +1,17 @@
+class fraction:
+    def __init__(self,n,d):
+        self.numerator = n
+        self.vinculum = "%"
+        self.denominator = d
+
+    def pulse(self):
+        ...
+
+    def minus(self):
+        ...
+
+    def multiple(self):
+        ...
+
+    def divide(self):
+        ...
