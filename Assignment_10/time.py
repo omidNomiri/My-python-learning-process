809c8734996dbb8b18cb49b4df8001b52c392d5e
omidNomiri <omidnomiri@gmail.com>
2023-11-21 19:50:34 +0330

    ADD time file

---
+++ Assignment_10/time.py
@@ -0,0 +1,12 @@
+class time:
+    def __init__(self,s,m,h):
+        self.seconds = s
+        self.minute = m
+        self.hours = h
+
+    def passing():
+        ...
+    
+    def happen():
+        ...
+    
