d448dc353841af00754de5ffea46782f7fc596b3
omidNomiri <omidnomiri@gmail.com>
2023-11-21 19:58:13 +0330

    ADD history file

---
+++ Assignment_10/history.py
@@ -0,0 +1,19 @@
+class history:
+    def __init__(self,y):
+        self.year = y
+    
+    def happen(self):
+        ...
+
+    def record(self):
+        ...
+
+    def repeat(self):
+        ...
+    
+    def rewrite(self):
+        ...
+    
+    def forget(self):
+        ...
+    
