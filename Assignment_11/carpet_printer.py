74c2fe6581e880e77941ef146666a6284858e8c1
omidNomiri <omidnomiri@gmail.com>
2023-11-22 21:28:51 +0330

    ADD carpet printer(just template...)

---
+++ Assignment_11/carpet_printer.py
@@ -0,0 +1,9 @@
+
+def carpet_printer(number):
+    carpet = []
+    for i in range(number):
+        row = []
+        for j in range(number):
+            pattern = ...
+            row.append(pattern)
+        carpet.append(row)
