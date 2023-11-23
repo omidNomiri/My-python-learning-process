e51c3768c6072df5aaa638717803c345d68fcf51
omidNomiri <omidnomiri@gmail.com>
2023-11-23 11:49:49 +0330

    FIX carpet printer(complete)

--- Assignment_11/carpet_printer.py
+++ Assignment_11/carpet_printer.py
@@ -1,9 +1,21 @@
-
 def carpet_printer(number):
     carpet = []
     for i in range(number):
         row = []
         for j in range(number):
-            pattern = ...
-            row.append(pattern)
+                pattern = min(i, j, number - i - 1, number - j - 1)
+                if pattern == 0:
+                    row.append("🟫")
+                elif (pattern % 2) != 0:
+                    row.append("🟩")
+                elif (pattern % 2) == 0:
+                    row.append("🟦")
+
         carpet.append(row)
+
+    for row in carpet:
+        for item in row:
+            print(item, end=" ")
+        print()
+
+carpet_printer(11)
