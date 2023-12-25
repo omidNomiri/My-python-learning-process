f44a75c0ebf4c233289994837a0da41dcf52a5c3
omidNomiri <omidnomiri@gmail.com>
2023-12-25 16:50:25 +0330

    style: code cleaned

--- Assignment_18/main.py
+++ Assignment_18/main.py
@@ -6,10 +6,10 @@ from functools import partial
 class Game:
     def __init__(self):
         self.mode = None
-        self.turn_player = 1
+        self.turn_player = "X"
         self.move = 1
-        self.player1_score = 0
-        self.player2_score = 0
+        self.playerO_score = 0
+        self.playerX_score = 0
         self.equal_score = 0
         self.main_window = loader.load("Assignment_18/main_window.ui")
         self.message_box = QMessageBox()
@@ -27,13 +27,13 @@ class Game:
             for column in range(3):
                 self.buttons[row][column].setText("")
                 self.buttons[row][column].setStyleSheet("background-color: rgb(232, 232, 232);")
-        self.player1_score = 0
-        self.player2_score = 0
+        self.playerO_score = 0
+        self.playerX_score = 0
         self.equal_score = 0
     
     def refresh_game(self):
-        self.main_window.btn_player1_score.setText(f"player 1 score: {self.player1_score}")
-        self.main_window.btn_player2_score.setText(f"player 2 score: {self.player2_score}")
+        self.main_window.btn_player1_score.setText(f"player X score: {self.playerX_score}")
+        self.main_window.btn_player2_score.setText(f"player O score: {self.playerO_score}")
         self.main_window.btn_equal.setText(f"equal: {self.equal_score} time")
         self.move = 0
         for row in range(3):
@@ -46,39 +46,41 @@ class Game:
 
     def play(self, row, column):
         if self.mode == "PvP":
-            if self.turn_player == 1:
+            if self.turn_player == "X":
                 self.buttons[row][column].setText("X")
                 self.buttons[row][column].setStyleSheet("color:Blue")
-                self.turn_player = 2
                 self.main_window.btn_turn_O.setStyleSheet("color:Red")
                 self.main_window.btn_turn_X.setStyleSheet("color:White")
-            elif self.turn_player == 2:
+                self.check_winner()
+                self.turn_player = "O"
+            elif self.turn_player == "O":
                 self.buttons[row][column].setText("O")
                 self.buttons[row][column].setStyleSheet("color:Red")
-                self.turn_player = 1
                 self.main_window.btn_turn_O.setStyleSheet("color:White")
                 self.main_window.btn_turn_X.setStyleSheet("color:Blue")
-            self.check_winner()
+                self.check_winner()
+                self.turn_player = "X"
 
         elif self.mode == "PvC":
-            if self.turn_player == 1:
+            if self.turn_player == "X":
                 self.buttons[row][column].setText("X")
                 self.buttons[row][column].setStyleSheet("color:Blue")
-                self.turn_player = 2
                 self.main_window.btn_turn_O.setStyleSheet("color:Red")
                 self.main_window.btn_turn_X.setStyleSheet("color:White")
-            elif self.turn_player == 2:
+                self.check_winner()
+                self.turn_player = "O"
+            elif self.turn_player == "O":
                 while True:
                     row = randint(0, 2)
                     column = randint(0, 2)
                     if self.buttons[row][column].text() == "":
                         self.buttons[row][column].setText("O")
                         self.buttons[row][column].setStyleSheet("color:Red")
-                        self.turn_player = 1
                         self.main_window.btn_turn_O.setStyleSheet("color:White")
                         self.main_window.btn_turn_X.setStyleSheet("color:Blue")
                         break
                 self.check_winner()
+                self.turn_player = "X"
 
         else:
             self.message_box.setWindowTitle("Error Game mode")
@@ -92,43 +94,43 @@ class Game:
             if self.buttons[row][0].text() == self.buttons[row][1].text() == self.buttons[row][2].text() != "":
                 if self.buttons[row][0].text() == "X":
                     self.show_message()
-                    self.player1_score += 1
+                    self.playerX_score += 1
                     self.refresh_game()
                     return
                 else:
                     self.show_message()
-                    self.player2_score += 1
+                    self.playerO_score += 1
                     self.refresh_game
                     return
             if self.buttons[0][row].text() == self.buttons[1][row].text() == self.buttons[2][row].text() != "":
                 if self.buttons[0][row].text() == "X":
                     self.show_message()
-                    self.player1_score += 1
+                    self.playerX_score += 1
                     self.refresh_game()
                     return
                 else:
                     self.show_message()
-                    self.player2_score += 1
+                    self.playerO_score += 1
                     self.refresh_game()
                     return
         if self.buttons[0][0].text() == self.buttons[1][1].text() == self.buttons[2][2].text() == "X":
             self.show_message()
-            self.player1_score += 1
+            self.playerX_score += 1
             self.refresh_game()
             return
         if self.buttons[0][2].text() == self.buttons[1][1].text() == self.buttons[2][0].text() == "X":
             self.show_message()
-            self.player1_score += 1
+            self.playerX_score += 1
             self.refresh_game()
             return
         if self.buttons[0][0].text() == self.buttons[1][1].text() == self.buttons[2][2].text() == "O":
             self.show_message()
-            self.player2_score += 1
+            self.playerO_score += 1
             self.refresh_game()
             return
         if self.buttons[0][2].text() == self.buttons[1][1].text() == self.buttons[2][0].text() == "O":  
             self.show_message()
-            self.player2_score += 1
+            self.playerO_score += 1
             self.refresh_game()
             return
         elif self.move == 9:
@@ -150,8 +152,8 @@ app = QApplication([])
 game = Game()
 
 game.main_window.btn_turn_O.setStyleSheet("color:White")
-game.main_window.btn_player1_score.setText(f"player 1 score: {game.player1_score}")
-game.main_window.btn_player2_score.setText(f"player 2 score: {game.player2_score}")
+game.main_window.btn_player1_score.setText(f"player X score: {game.playerX_score}")
+game.main_window.btn_player2_score.setText(f"player O score: {game.playerO_score}")
 game.main_window.btn_equal.setText(f"equal: {game.equal_score} time")
 
 game.main_window.player_vs_player.clicked.connect(partial(game.select_mode, "PvP"))
