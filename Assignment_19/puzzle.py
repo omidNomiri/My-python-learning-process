b7974199957abe0954e1ae8447cf2499fdbf2ca2
omidNomiri <omidnomiri@gmail.com>
2023-12-29 18:20:41 +0330

    Feat: block move

--- Assignment_19/puzzle.py
+++ Assignment_19/puzzle.py
@@ -1,4 +1,7 @@
+from numbers import Number
 import sys
+from functools import partial
+from random import randint
 from PySide6.QtWidgets import QApplication, QMainWindow
 from ui_main_window import Ui_MainWindow
 
@@ -7,12 +10,43 @@ class Game(QMainWindow):
         super().__init__()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
+        self.unique_list = []
         self.buttons = [
             [self.ui.btn_1, self.ui.btn_2, self.ui.btn_3, self.ui.btn_4],
             [self.ui.btn_5, self.ui.btn_6, self.ui.btn_7, self.ui.btn_8],
             [self.ui.btn_9, self.ui.btn_10, self.ui.btn_11, self.ui.btn_12],
             [self.ui.btn_13, self.ui.btn_14, self.ui.btn_15, self.ui.btn_16]
         ]
+
+        while len(self.unique_list) < 16:
+            random_number = randint(1, 16)
+            if random_number not in self.unique_list:
+                self.unique_list.append(random_number)
+
+        self.unique_list_2D = [self.unique_list[i:i+4] for i in range(0, len(self.unique_list), 4)]
+
+        for i in range(4):
+            for j in range(4):
+                number = self.unique_list_2D[i][j]
+                self.buttons[i][j].setText(str(number))
+                self.buttons[i][j].clicked.connect(partial(self.play, i, j))
+                if number == 16:
+                    self.buttons[i][j].setVisible(False)
+                    self.empty_i = i
+                    self.empty_j = j
+
+    def play(self, i, j):
+        if (i == self.empty_i and abs(j - self.empty_j) == 1 and j != self.empty_j) or \
+            (j == self.empty_j and abs(i - self.empty_i) == 1 and i != self.empty_i):
+
+            number = int(self.buttons[i][j].text())
+            self.buttons[self.empty_i][self.empty_j].setText(str(number))
+            self.buttons[i][j].setText("16")
+
+            self.buttons[self.empty_i][self.empty_j].setVisible(True)
+            self.buttons[i][j].setVisible(False)
+            self.empty_i = i
+            self.empty_j = j
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
--- Assignment_19/ui_main_window.py
+++ Assignment_19/ui_main_window.py
@@ -22,7 +22,7 @@ class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         if not MainWindow.objectName():
             MainWindow.setObjectName(u"MainWindow")
-        MainWindow.resize(411, 406)
+        MainWindow.resize(385, 406)
         sizePolicy = QSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
@@ -33,15 +33,15 @@ class Ui_MainWindow(object):
         self.centralwidget.setObjectName(u"centralwidget")
         self.gridLayout = QGridLayout(self.centralwidget)
         self.gridLayout.setObjectName(u"gridLayout")
-        self.btn_1 = QPushButton(self.centralwidget)
-        self.btn_1.setObjectName(u"btn_1")
-        sizePolicy.setHeightForWidth(self.btn_1.sizePolicy().hasHeightForWidth())
-        self.btn_1.setSizePolicy(sizePolicy)
-        self.btn_1.setStyleSheet(u"background-color: rgb(121, 238, 113);\n"
+        self.btn_9 = QPushButton(self.centralwidget)
+        self.btn_9.setObjectName(u"btn_9")
+        sizePolicy.setHeightForWidth(self.btn_9.sizePolicy().hasHeightForWidth())
+        self.btn_9.setSizePolicy(sizePolicy)
+        self.btn_9.setStyleSheet(u"background-color: rgb(121, 238, 113);\n"
 "color: rgb(0, 0, 0);\n"
 "border-radius: 15px;")
 
-        self.gridLayout.addWidget(self.btn_1, 0, 0, 1, 1)
+        self.gridLayout.addWidget(self.btn_9, 2, 1, 1, 1)
 
         self.btn_3 = QPushButton(self.centralwidget)
         self.btn_3.setObjectName(u"btn_3")
@@ -51,117 +51,7 @@ class Ui_MainWindow(object):
 "color: rgb(0, 0, 0);\n"
 "border-radius: 15px;")
 
-        self.gridLayout.addWidget(self.btn_3, 1, 0, 1, 1)
-
-        self.btn_16 = QPushButton(self.centralwidget)
-        self.btn_16.setObjectName(u"btn_16")
-        sizePolicy.setHeightForWidth(self.btn_16.sizePolicy().hasHeightForWidth())
-        self.btn_16.setSizePolicy(sizePolicy)
-        self.btn_16.setStyleSheet(u"background-color: rgb(121, 238, 113);\n"
-"color: rgb(0, 0, 0);\n"
-"border-radius: 15px;")
-
-        self.gridLayout.addWidget(self.btn_16, 0, 2, 1, 1)
-
-        self.btn_2 = QPushButton(self.centralwidget)
-        self.btn_2.setObjectName(u"btn_2")
-        sizePolicy.setHeightForWidth(self.btn_2.sizePolicy().hasHeightForWidth())
-        self.btn_2.setSizePolicy(sizePolicy)
-        self.btn_2.setStyleSheet(u"background-color: rgb(121, 238, 113);\n"
-"color: rgb(0, 0, 0);\n"
-"border-radius: 15px;")
-
-        self.gridLayout.addWidget(self.btn_2, 0, 3, 1, 1)
-
-        self.btn_14 = QPushButton(self.centralwidget)
-        self.btn_14.setObjectName(u"btn_14")
-        sizePolicy.setHeightForWidth(self.btn_14.sizePolicy().hasHeightForWidth())
-        self.btn_14.setSizePolicy(sizePolicy)
-        self.btn_14.setStyleSheet(u"background-color: rgb(121, 238, 113);\n"
-"color: rgb(0, 0, 0);\n"
-"border-radius: 15px;")
-
-        self.gridLayout.addWidget(self.btn_14, 1, 2, 1, 1)
-
-        self.btn_4 = QPushButton(self.centralwidget)
-        self.btn_4.setObjectName(u"btn_4")
-        sizePolicy.setHeightForWidth(self.btn_4.sizePolicy().hasHeightForWidth())
-        self.btn_4.setSizePolicy(sizePolicy)
-        self.btn_4.setStyleSheet(u"background-color: rgb(121, 238, 113);\n"
-"color: rgb(0, 0, 0);\n"
-"border-radius: 15px;")
-
-        self.gridLayout.addWidget(self.btn_4, 1, 3, 1, 1)
-
-        self.btn_5 = QPushButton(self.centralwidget)
-        self.btn_5.setObjectName(u"btn_5")
-        sizePolicy.setHeightForWidth(self.btn_5.sizePolicy().hasHeightForWidth())
-        self.btn_5.setSizePolicy(sizePolicy)
-        self.btn_5.setStyleSheet(u"background-color: rgb(121, 238, 113);\n"
-"color: rgb(0, 0, 0);\n"
-"border-radius: 15px;")
-
-        self.gridLayout.addWidget(self.btn_5, 3, 0, 1, 1)
-
-        self.btn_8 = QPushButton(self.centralwidget)
-        self.btn_8.setObjectName(u"btn_8")
-        sizePolicy.setHeightForWidth(self.btn_8.sizePolicy().hasHeightForWidth())
-        self.btn_8.setSizePolicy(sizePolicy)
-        self.btn_8.setStyleSheet(u"background-color: rgb(121, 238, 113);\n"
-"color: rgb(0, 0, 0);\n"
-"border-radius: 15px;")
-
-        self.gridLayout.addWidget(self.btn_8, 3, 2, 1, 1)
-
-        self.btn_6 = QPushButton(self.centralwidget)
-        self.btn_6.setObjectName(u"btn_6")
-        sizePolicy.setHeightForWidth(self.btn_6.sizePolicy().hasHeightForWidth())
-        self.btn_6.setSizePolicy(sizePolicy)
-        self.btn_6.setStyleSheet(u"background-color: rgb(121, 238, 113);\n"
-"color: rgb(0, 0, 0);\n"
-"border-radius: 15px;")
-
-        self.gridLayout.addWidget(self.btn_6, 3, 3, 1, 1)
-
-        self.btn_12 = QPushButton(self.centralwidget)
-        self.btn_12.setObjectName(u"btn_12")
-        sizePolicy.setHeightForWidth(self.btn_12.sizePolicy().hasHeightForWidth())
-        self.btn_12.setSizePolicy(sizePolicy)
-        self.btn_12.setStyleSheet(u"background-color: rgb(121, 238, 113);\n"
-"color: rgb(0, 0, 0);\n"
-"border-radius: 15px;")
-
-        self.gridLayout.addWidget(self.btn_12, 1, 4, 1, 1)
-
-        self.btn_11 = QPushButton(self.centralwidget)
-        self.btn_11.setObjectName(u"btn_11")
-        sizePolicy.setHeightForWidth(self.btn_11.sizePolicy().hasHeightForWidth())
-        self.btn_11.setSizePolicy(sizePolicy)
-        self.btn_11.setStyleSheet(u"background-color: rgb(121, 238, 113);\n"
-"color: rgb(0, 0, 0);\n"
-"border-radius: 15px;")
-
-        self.gridLayout.addWidget(self.btn_11, 3, 4, 1, 1)
-
-        self.btn_15 = QPushButton(self.centralwidget)
-        self.btn_15.setObjectName(u"btn_15")
-        sizePolicy.setHeightForWidth(self.btn_15.sizePolicy().hasHeightForWidth())
-        self.btn_15.setSizePolicy(sizePolicy)
-        self.btn_15.setStyleSheet(u"background-color: rgb(121, 238, 113);\n"
-"color: rgb(0, 0, 0);\n"
-"border-radius: 15px;")
-
-        self.gridLayout.addWidget(self.btn_15, 0, 4, 1, 1)
-
-        self.btn_10 = QPushButton(self.centralwidget)
-        self.btn_10.setObjectName(u"btn_10")
-        sizePolicy.setHeightForWidth(self.btn_10.sizePolicy().hasHeightForWidth())
-        self.btn_10.setSizePolicy(sizePolicy)
-        self.btn_10.setStyleSheet(u"background-color: rgb(121, 238, 113);\n"
-"color: rgb(0, 0, 0);\n"
-"border-radius: 15px;")
-
-        self.gridLayout.addWidget(self.btn_10, 5, 2, 1, 1)
+        self.gridLayout.addWidget(self.btn_3, 0, 4, 1, 1)
 
         self.btn_7 = QPushButton(self.centralwidget)
         self.btn_7.setObjectName(u"btn_7")
@@ -171,7 +61,117 @@ class Ui_MainWindow(object):
 "color: rgb(0, 0, 0);\n"
 "border-radius: 15px;")
 
-        self.gridLayout.addWidget(self.btn_7, 5, 3, 1, 1)
+        self.gridLayout.addWidget(self.btn_7, 1, 4, 1, 1)
+
+        self.btn_8 = QPushButton(self.centralwidget)
+        self.btn_8.setObjectName(u"btn_8")
+        sizePolicy.setHeightForWidth(self.btn_8.sizePolicy().hasHeightForWidth())
+        self.btn_8.setSizePolicy(sizePolicy)
+        self.btn_8.setStyleSheet(u"background-color: rgb(121, 238, 113);\n"
+"color: rgb(0, 0, 0);\n"
+"border-radius: 15px;")
+
+        self.gridLayout.addWidget(self.btn_8, 1, 6, 1, 1)
+
+        self.btn_2 = QPushButton(self.centralwidget)
+        self.btn_2.setObjectName(u"btn_2")
+        sizePolicy.setHeightForWidth(self.btn_2.sizePolicy().hasHeightForWidth())
+        self.btn_2.setSizePolicy(sizePolicy)
+        self.btn_2.setStyleSheet(u"background-color: rgb(121, 238, 113);\n"
+"color: rgb(0, 0, 0);\n"
+"border-radius: 15px;")
+
+        self.gridLayout.addWidget(self.btn_2, 0, 2, 1, 1)
+
+        self.btn_1 = QPushButton(self.centralwidget)
+        self.btn_1.setObjectName(u"btn_1")
+        sizePolicy.setHeightForWidth(self.btn_1.sizePolicy().hasHeightForWidth())
+        self.btn_1.setSizePolicy(sizePolicy)
+        self.btn_1.setStyleSheet(u"background-color: rgb(121, 238, 113);\n"
+"color: rgb(0, 0, 0);\n"
+"border-radius: 15px;")
+
+        self.gridLayout.addWidget(self.btn_1, 0, 1, 1, 1)
+
+        self.btn_6 = QPushButton(self.centralwidget)
+        self.btn_6.setObjectName(u"btn_6")
+        sizePolicy.setHeightForWidth(self.btn_6.sizePolicy().hasHeightForWidth())
+        self.btn_6.setSizePolicy(sizePolicy)
+        self.btn_6.setStyleSheet(u"background-color: rgb(121, 238, 113);\n"
+"color: rgb(0, 0, 0);\n"
+"border-radius: 15px;")
+
+        self.gridLayout.addWidget(self.btn_6, 1, 2, 1, 1)
+
+        self.btn_5 = QPushButton(self.centralwidget)
+        self.btn_5.setObjectName(u"btn_5")
+        sizePolicy.setHeightForWidth(self.btn_5.sizePolicy().hasHeightForWidth())
+        self.btn_5.setSizePolicy(sizePolicy)
+        self.btn_5.setStyleSheet(u"background-color: rgb(121, 238, 113);\n"
+"color: rgb(0, 0, 0);\n"
+"border-radius: 15px;")
+
+        self.gridLayout.addWidget(self.btn_5, 1, 1, 1, 1)
+
+        self.btn_4 = QPushButton(self.centralwidget)
+        self.btn_4.setObjectName(u"btn_4")
+        sizePolicy.setHeightForWidth(self.btn_4.sizePolicy().hasHeightForWidth())
+        self.btn_4.setSizePolicy(sizePolicy)
+        self.btn_4.setStyleSheet(u"background-color: rgb(121, 238, 113);\n"
+"color: rgb(0, 0, 0);\n"
+"border-radius: 15px;")
+
+        self.gridLayout.addWidget(self.btn_4, 0, 6, 1, 1)
+
+        self.btn_10 = QPushButton(self.centralwidget)
+        self.btn_10.setObjectName(u"btn_10")
+        sizePolicy.setHeightForWidth(self.btn_10.sizePolicy().hasHeightForWidth())
+        self.btn_10.setSizePolicy(sizePolicy)
+        self.btn_10.setStyleSheet(u"background-color: rgb(121, 238, 113);\n"
+"color: rgb(0, 0, 0);\n"
+"border-radius: 15px;")
+
+        self.gridLayout.addWidget(self.btn_10, 2, 2, 1, 1)
+
+        self.btn_15 = QPushButton(self.centralwidget)
+        self.btn_15.setObjectName(u"btn_15")
+        sizePolicy.setHeightForWidth(self.btn_15.sizePolicy().hasHeightForWidth())
+        self.btn_15.setSizePolicy(sizePolicy)
+        self.btn_15.setStyleSheet(u"background-color: rgb(121, 238, 113);\n"
+"color: rgb(0, 0, 0);\n"
+"border-radius: 15px;")
+
+        self.gridLayout.addWidget(self.btn_15, 5, 4, 1, 1)
+
+        self.btn_12 = QPushButton(self.centralwidget)
+        self.btn_12.setObjectName(u"btn_12")
+        sizePolicy.setHeightForWidth(self.btn_12.sizePolicy().hasHeightForWidth())
+        self.btn_12.setSizePolicy(sizePolicy)
+        self.btn_12.setStyleSheet(u"background-color: rgb(121, 238, 113);\n"
+"color: rgb(0, 0, 0);\n"
+"border-radius: 15px;")
+
+        self.gridLayout.addWidget(self.btn_12, 2, 6, 1, 1)
+
+        self.btn_16 = QPushButton(self.centralwidget)
+        self.btn_16.setObjectName(u"btn_16")
+        sizePolicy.setHeightForWidth(self.btn_16.sizePolicy().hasHeightForWidth())
+        self.btn_16.setSizePolicy(sizePolicy)
+        self.btn_16.setStyleSheet(u"background-color: rgb(121, 238, 113);\n"
+"color: rgb(0, 0, 0);\n"
+"border-radius: 15px;")
+
+        self.gridLayout.addWidget(self.btn_16, 5, 6, 1, 1)
+
+        self.btn_11 = QPushButton(self.centralwidget)
+        self.btn_11.setObjectName(u"btn_11")
+        sizePolicy.setHeightForWidth(self.btn_11.sizePolicy().hasHeightForWidth())
+        self.btn_11.setSizePolicy(sizePolicy)
+        self.btn_11.setStyleSheet(u"background-color: rgb(121, 238, 113);\n"
+"color: rgb(0, 0, 0);\n"
+"border-radius: 15px;")
+
+        self.gridLayout.addWidget(self.btn_11, 2, 4, 1, 1)
 
         self.btn_13 = QPushButton(self.centralwidget)
         self.btn_13.setObjectName(u"btn_13")
@@ -181,17 +181,17 @@ class Ui_MainWindow(object):
 "color: rgb(0, 0, 0);\n"
 "border-radius: 15px;")
 
-        self.gridLayout.addWidget(self.btn_13, 5, 4, 1, 1)
+        self.gridLayout.addWidget(self.btn_13, 5, 1, 1, 1)
 
-        self.btn_9 = QPushButton(self.centralwidget)
-        self.btn_9.setObjectName(u"btn_9")
-        sizePolicy.setHeightForWidth(self.btn_9.sizePolicy().hasHeightForWidth())
-        self.btn_9.setSizePolicy(sizePolicy)
-        self.btn_9.setStyleSheet(u"background-color: rgb(121, 238, 113);\n"
+        self.btn_14 = QPushButton(self.centralwidget)
+        self.btn_14.setObjectName(u"btn_14")
+        sizePolicy.setHeightForWidth(self.btn_14.sizePolicy().hasHeightForWidth())
+        self.btn_14.setSizePolicy(sizePolicy)
+        self.btn_14.setStyleSheet(u"background-color: rgb(121, 238, 113);\n"
 "color: rgb(0, 0, 0);\n"
 "border-radius: 15px;")
 
-        self.gridLayout.addWidget(self.btn_9, 5, 0, 1, 1)
+        self.gridLayout.addWidget(self.btn_14, 5, 2, 1, 1)
 
         MainWindow.setCentralWidget(self.centralwidget)
 
@@ -202,21 +202,21 @@ class Ui_MainWindow(object):
 
     def retranslateUi(self, MainWindow):
         MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
-        self.btn_1.setText("")
-        self.btn_3.setText("")
-        self.btn_16.setText("")
-        self.btn_2.setText("")
-        self.btn_14.setText("")
-        self.btn_4.setText("")
-        self.btn_5.setText("")
-        self.btn_8.setText("")
-        self.btn_6.setText("")
-        self.btn_12.setText("")
-        self.btn_11.setText("")
-        self.btn_15.setText("")
-        self.btn_10.setText("")
-        self.btn_7.setText("")
-        self.btn_13.setText("")
         self.btn_9.setText("")
+        self.btn_3.setText("")
+        self.btn_7.setText("")
+        self.btn_8.setText("")
+        self.btn_2.setText("")
+        self.btn_1.setText("")
+        self.btn_6.setText("")
+        self.btn_5.setText("")
+        self.btn_4.setText("")
+        self.btn_10.setText("")
+        self.btn_15.setText("")
+        self.btn_12.setText("")
+        self.btn_16.setText("")
+        self.btn_11.setText("")
+        self.btn_13.setText("")
+        self.btn_14.setText("")
     # retranslateUi
 
