3a01959be124388acdcd4998f43149299f06ab1c
omidNomiri <omidnomiri@gmail.com>
2023-12-30 22:14:53 +0330

    Add: Translator App

---
+++ Assignment_19/translator/main.py
@@ -0,0 +1,77 @@
+import sys
+from functools import partial
+import gtts
+from PySide6.QtWidgets import QApplication, QMainWindow,QMessageBox
+from ui_main_window import Ui_MainWindow
+
+class Translator(QMainWindow):
+     def __init__(self):
+          super().__init__()
+          with open("Assignment_19/translator/Translate.txt","r") as database_translate:
+               temp = database_translate.read().split("\n")
+               self.words_bank = []
+               for line in range(0,len(temp) - 1,2):
+                   my_dictionary = {"english":temp[line],"persian":temp[line+1]}
+                   self.words_bank.append(my_dictionary)
+          self.message_box = QMessageBox()
+          self.ui = Ui_MainWindow()
+          self.ui.setupUi(self)
+
+     def choice_mode(self, mode_translate):
+          self.mode = mode_translate
+
+     def translate(self):
+          if not self.mode:
+               self.message_box.setWindowTitle("Error")
+               self.message_box.setText("Please select your translate language.")
+               self.message_box.exec()
+               return
+          elif self.mode == "Persian to English":
+               input_text = self.ui.input_text.text()
+               input_words = input_text.split(" ")
+
+               output_words = []
+               for input_word in input_words:
+                  translated_word = self.translate_persian_word(input_word)
+                  output_words.append(translated_word)
+
+               output_text = " ".join(output_words)
+               self.ui.output_text.setText(output_text)
+
+          elif self.mode == "English to Persian":
+               input_text = self.ui.input_text.text()
+               input_words = input_text.split(" ")
+
+               output_words = []
+               for input_word in input_words:
+                  translated_word = self.translate_english_word(input_word)
+                  output_words.append(translated_word)
+
+               output_text = " ".join(output_words)
+               self.ui.output_text.setText(output_text)
+
+     def translate_persian_word(self, input_word):
+          for word in self.words_bank:
+               if input_word == word["persian"]:
+                    return word["english"]
+          return input_word
+
+     def translate_english_word(self, input_word):
+          for word in self.words_bank:
+               if input_word == word["english"]:
+                    return word["persian"]
+          return input_word
+
+if __name__ == "__main__":
+     app = QApplication(sys.argv)
+     
+     window = Translator()
+     
+     window.ui.rdbtn_pr_en.clicked.connect(partial(window.choice_mode, "Persian to English"))
+     window.ui.rdbtn_en_pr.clicked.connect(partial(window.choice_mode, "English to Persian"))
+
+     window.ui.btn_translate.clicked.connect(partial(window.translate))
+
+     window.show()
+
+     app.exec()
---
+++ Assignment_19/translator/ui_main_window.py
@@ -0,0 +1,117 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'main_window.ui'
+##
+## Created by: Qt User Interface Compiler version 6.6.1
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+    QFont, QFontDatabase, QGradient, QIcon,
+    QImage, QKeySequence, QLinearGradient, QPainter,
+    QPalette, QPixmap, QRadialGradient, QTransform)
+from PySide6.QtWidgets import (QApplication, QGridLayout, QLineEdit, QMainWindow,
+    QPushButton, QRadioButton, QSizePolicy, QWidget)
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        if not MainWindow.objectName():
+            MainWindow.setObjectName(u"MainWindow")
+        MainWindow.resize(416, 533)
+        sizePolicy = QSizePolicy(QSizePolicy.Preferred, QSizePolicy.Preferred)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(MainWindow.sizePolicy().hasHeightForWidth())
+        MainWindow.setSizePolicy(sizePolicy)
+        MainWindow.setStyleSheet(u"background-color: rgb(99, 99, 99);")
+        self.centralwidget = QWidget(MainWindow)
+        self.centralwidget.setObjectName(u"centralwidget")
+        self.gridLayout = QGridLayout(self.centralwidget)
+        self.gridLayout.setObjectName(u"gridLayout")
+        self.output_text = QLineEdit(self.centralwidget)
+        self.output_text.setObjectName(u"output_text")
+        sizePolicy.setHeightForWidth(self.output_text.sizePolicy().hasHeightForWidth())
+        self.output_text.setSizePolicy(sizePolicy)
+        self.output_text.setStyleSheet(u"background-color: rgb(194, 194, 194);\n"
+"border: 2px solid white;\n"
+"border-radius: 15px;")
+
+        self.gridLayout.addWidget(self.output_text, 6, 0, 1, 2)
+
+        self.btn_speak_output = QPushButton(self.centralwidget)
+        self.btn_speak_output.setObjectName(u"btn_speak_output")
+        self.btn_speak_output.setStyleSheet(u"background-color: rgb(135, 135, 135);\n"
+"border: 2px solid black;\n"
+"border-radius: 10px;")
+
+        self.gridLayout.addWidget(self.btn_speak_output, 5, 1, 1, 1)
+
+        self.rdbtn_en_pr = QRadioButton(self.centralwidget)
+        self.rdbtn_en_pr.setObjectName(u"rdbtn_en_pr")
+        self.rdbtn_en_pr.setStyleSheet(u"background-color: rgb(148, 148, 148);\n"
+"border: 2px solid black;\n"
+"border-radius: 10px;")
+
+        self.gridLayout.addWidget(self.rdbtn_en_pr, 2, 0, 1, 1)
+
+        self.rdbtn_pr_en = QRadioButton(self.centralwidget)
+        self.rdbtn_pr_en.setObjectName(u"rdbtn_pr_en")
+        self.rdbtn_pr_en.setStyleSheet(u"background-color: rgb(148, 148, 148);\n"
+"border: 2px solid black;\n"
+"border-radius: 10px;")
+
+        self.gridLayout.addWidget(self.rdbtn_pr_en, 2, 1, 1, 1)
+
+        self.btn_speak_input = QPushButton(self.centralwidget)
+        self.btn_speak_input.setObjectName(u"btn_speak_input")
+        self.btn_speak_input.setStyleSheet(u"background-color: rgb(135, 135, 135);\n"
+"border: 2px solid black;\n"
+"border-radius: 10px;")
+
+        self.gridLayout.addWidget(self.btn_speak_input, 4, 1, 1, 1)
+
+        self.btn_translate = QPushButton(self.centralwidget)
+        self.btn_translate.setObjectName(u"btn_translate")
+        sizePolicy1 = QSizePolicy(QSizePolicy.Minimum, QSizePolicy.Expanding)
+        sizePolicy1.setHorizontalStretch(0)
+        sizePolicy1.setVerticalStretch(0)
+        sizePolicy1.setHeightForWidth(self.btn_translate.sizePolicy().hasHeightForWidth())
+        self.btn_translate.setSizePolicy(sizePolicy1)
+        self.btn_translate.setStyleSheet(u"background-color: rgb(135, 135, 135);\n"
+"border: 2px solid black;\n"
+"border-radius: 15px;")
+
+        self.gridLayout.addWidget(self.btn_translate, 4, 0, 2, 1)
+
+        self.input_text = QLineEdit(self.centralwidget)
+        self.input_text.setObjectName(u"input_text")
+        sizePolicy.setHeightForWidth(self.input_text.sizePolicy().hasHeightForWidth())
+        self.input_text.setSizePolicy(sizePolicy)
+        self.input_text.setStyleSheet(u"background-color: rgb(194, 194, 194);\n"
+"border: 2px solid white;\n"
+"border-radius: 15px;")
+
+        self.gridLayout.addWidget(self.input_text, 3, 0, 1, 2)
+
+        MainWindow.setCentralWidget(self.centralwidget)
+
+        self.retranslateUi(MainWindow)
+
+        QMetaObject.connectSlotsByName(MainWindow)
+    # setupUi
+
+    def retranslateUi(self, MainWindow):
+        MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
+        self.btn_speak_output.setText(QCoreApplication.translate("MainWindow", u"Speak english", None))
+        self.rdbtn_en_pr.setText(QCoreApplication.translate("MainWindow", u"English to Persian", None))
+        self.rdbtn_pr_en.setText(QCoreApplication.translate("MainWindow", u"Persian to English", None))
+        self.btn_speak_input.setText(QCoreApplication.translate("MainWindow", u"Speak persian", None))
+        self.btn_translate.setText(QCoreApplication.translate("MainWindow", u"Translate", None))
+        self.input_text.setText("")
+    # retranslateUi
+
