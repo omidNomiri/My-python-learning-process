552a3ddf782f793a1afda44bfb6a48e540d23afd
omidNomiri <omidnomiri@gmail.com>
2024-01-14 18:05:40 +0330

    Style,Feat: add some space line and write simple error handling

--- Assignment_21/main.py
+++ Assignment_21/main.py
@@ -8,59 +8,96 @@ class Store:
           self.factor = []
 
      def add_item(self):
-          product_name = str(input("name of item: "))
-          product_price = float(input("price of item: "))
-          product_count = int(input("how much you have: "))
-          self.curser.execute("INSERT INTO Products(name, price, storage) VALUES (?, ?, ?)", (product_name, product_price, product_count))
-          self.data_base.commit()
+          try:
+               product_name = str(input("name of item: "))
+               product_price = float(input("price of item: "))
+               product_count = int(input("how much you have: "))
+
+               self.curser.execute("INSERT INTO Products(name, price, storage) VALUES (?, ?, ?)", (product_name, product_price, product_count))
+               self.data_base.commit()
+
+               print("item adding successfully")
+          except Exception as error:
+               print(f"add item error: {error}")
 
      def edit_item(self):
-          self.show_item_list()
-          target_item_id = int(input("please enter your product id: "))
-          edit_target = int(input("what you want edit \n 1.name\n 2.price\n 3.storage\nyour choice: "))
-          if edit_target in (1,2,3):
-               new_value = input("please enter your new value: ")
-               if edit_target == 1:
-                    self.curser.execute("UPDATE Products SET name=? Where id=?",(new_value, target_item_id))
-               elif edit_target == 2:
-                    self.curser.execute("UPDATE Products SET price=? Where id=?", (new_value, target_item_id))
-               elif edit_target == 3:
-                    self.curser.execute("UPDATE Products SET storage=? Where id=?", (new_value, target_item_id))
-               self.data_base.commit()
+          try:
+               self.show_item_list()
+               target_item_id = int(input("please enter your product id: "))
+               edit_target = int(input("what you want edit \n 1.name\n 2.price\n 3.storage\nyour choice: "))
+               
+               if edit_target in (1,2,3):
+                    new_value = input("please enter your new value: ")
+                    if edit_target == 1:
+                         self.curser.execute("UPDATE Products SET name=? Where id=?",(new_value, target_item_id))
+                    elif edit_target == 2:
+                         self.curser.execute("UPDATE Products SET price=? Where id=?", (new_value, target_item_id))
+                    elif edit_target == 3:
+                         self.curser.execute("UPDATE Products SET storage=? Where id=?", (new_value, target_item_id))
+                    
+                    self.data_base.commit()
+                    print("Edit item successfully")
+               else:
+                    print("Invalid choice.")
+          except Exception as error:
+               print(f"Edit item error: {error}")
 
      def remove_item(self):
-          self.show_item_list()
-          target_item_id = input("please enter your product id: ")
-          self.curser.execute("DELETE FROM Products WHERE id=?", (target_item_id))
-          self.data_base.commit()
+          try:
+               self.show_item_list()
+               target_item_id = input("please enter your product id: ")
+
+               self.curser.execute("DELETE FROM Products WHERE id=?", (target_item_id))
+               self.data_base.commit()
+               print("Item removed successfully")
+          except Exception as error:
+               print(f"remove item error: {error}")
 
      def search_item(self):
-          target_item_id = input("Please enter your product name or product id: ")
-          items = self.curser.execute("SELECT * FROM Products WHERE id=? OR name=?", (target_item_id,target_item_id)).fetchall()
-          for item in items:
-               print(f"ID: {item[0]}, Name: {item[1]}, Price: {item[2]}, Storage: {item[3]}")
+          try:
+               target_item_id = input("Please enter your product name or product id: ")
+               items = self.curser.execute("SELECT * FROM Products WHERE id=? OR name=?", (target_item_id,target_item_id)).fetchall()
+
+               for item in items:
+                    print(f"ID: {item[0]}, Name: {item[1]}, Price: {item[2]}, Storage: {item[3]}")
+          except Exception as e:
+               print(f"Error in searching: {e}")
 
      def show_item_list(self):
-          items = self.curser.execute("SELECT * FROM Products").fetchall()
-          for item in items:
-               print(f"ID: {item[0]}, Name: {item[1]}, Price: {item[2]}, Storage: {item[3]}")
+          try:
+               items = self.curser.execute("SELECT * FROM Products").fetchall()
+
+               for item in items:
+                    print(f"ID: {item[0]}, Name: {item[1]}, Price: {item[2]}, Storage: {item[3]}")
+          except Exception as e:
+               print(f"Error in displaying: {e}")
 
      def buy(self):
-          while True:
-               self.show_item_list()
-               item_customer_want = input("Please enter your product name or product id: ")
-               how_many_item = int(input("How many: "))
-               self.curser.execute("UPDATE Products SET storage= storage - ? WHERE id=? OR name=?", (how_many_item, item_customer_want, item_customer_want))
-               item = self.curser.execute("SELECT * FROM Products WHERE id=? OR name=?", (item_customer_want, item_customer_want)).fetchall()
-               item = item[0]
-               list_for_factor = [str(value) for value in item]
-               self.factor.append(list_for_factor)
-               self.data_base.commit()
-               continue_shopping = input("Do you want to continue shopping? (y/n): ")
-               if continue_shopping.lower() != "y":
-                    for item in self.factor:
-                         print(item)
-                    break
+          try:
+               while True:
+                    self.show_item_list()
+                    item_customer_want = input("Please enter your product name or product id: ")
+                    how_many_item = int(input("How many: "))
+                    self.curser.execute("UPDATE Products SET storage= storage - ? WHERE id=? OR name=?", (how_many_item, item_customer_want, item_customer_want))
+                    item = self.curser.execute("SELECT * FROM Products WHERE id=? OR name=?", (item_customer_want, item_customer_want)).fetchall()
+
+                    if item:
+                         item = item[0]
+                         list_for_factor = [str(value) for value in item]
+                         self.factor.append(list_for_factor)
+                         self.data_base.commit()
+                         print("Item successfully add in your basket")
+
+                         continue_shopping = input("Do you want to continue shopping? (y/n): ")
+                         if continue_shopping.lower() != "y":
+                              for item in self.factor:
+                                   print(item)
+                              break
+                    else:
+                         print("Item not found")
+                         break
+          except Exception as e:
+               print(f"Error in buy item: {e}")
 
 app = Store()
 
