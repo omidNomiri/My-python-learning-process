01e95b10341d9bc85476cff47b46d4a8a534a281
omidNomiri <omidnomiri@gmail.com>
2024-01-18 16:48:07 +0330

    Fix: task now display

--- Assignment_22/main.py
+++ Assignment_22/main.py
@@ -9,6 +9,16 @@ class MainWindow(QMainWindow):
           super().__init__()
           self.ui = Ui_MainWindow()
           self.ui.setupUi(self)
+          self.db = Database()
+          self.tasks = self.db.get_task()
+
+          for i in range(len(self.tasks)):
+               new_task_box = QCheckBox()
+               new_label = QLabel()
+               new_label.setText(self.tasks[i][1])
+
+               self.ui.form_Layout.addWidget(new_task_box)
+               self.ui.form_Layout.addWidget(new_label)
 
 if __name__ == "__main__":
      app = QApplication(sys.argv)
