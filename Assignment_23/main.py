a9a78279388ffa48e1b203e298126bc511a76112
omidNomiri <omidnomiri@gmail.com>
2024-02-01 17:08:23 +0330

    feat : add help ,about ,solve menu and add exit button

--- Assignment_23/main.py
+++ Assignment_23/main.py
@@ -3,115 +3,141 @@ from random import randint
 from sudoku import Sudoku
 from functools import partial
 import PySide6.QtCore
-from PySide6.QtWidgets import QMainWindow ,QApplication ,QLineEdit ,QFileDialog
+from PySide6.QtWidgets import QMainWindow ,QApplication ,QLineEdit ,QFileDialog ,QMessageBox
 from ui_main_window import Ui_MainWindow
 
 class Sudoku_game(QMainWindow):
-     def __init__(self):
-          super().__init__()
-          self.ui = Ui_MainWindow()
-          self.ui.setupUi(self)
-          self.mode = 0
-          self.win = False
+    def __init__(self):
+        super().__init__()
+        self.ui = Ui_MainWindow()
+        self.msg_box = QMessageBox()
+        self.ui.setupUi(self)
+        self.mode = 0
+        self.win = False
+        self.puzzle = Sudoku(3, randint(0 ,1000)).difficulty(0.5)
 
-          self.ui.new_game_menu.triggered.connect(self.new_game)
-          self.ui.open_file_menu.triggered.connect(self.open_file)
-          self.ui.dark_mode_toggle_btn.clicked.connect(partial(self.theme_toggle))
-          self.ui.dark_mode_toggle_btn.setText("🌑")
-          self.line_text = [[None for row in range(9)] for column in range(9)]
+        self.ui.new_game_menu.triggered.connect(self.new_game)
+        self.ui.open_file_menu.triggered.connect(self.open_file)
+        self.ui.solve_menu.triggered.connect(self.solve)
+        self.ui.about_menu.triggered.connect(self.about)
+        self.ui.help_menu.triggered.connect(self.help)
 
-          for row in range(9):
-               for column in range(9):
-                    new_line_edit = QLineEdit()
-                    self.ui.grid_layout.addWidget(new_line_edit, row, column)
-                    new_line_edit.setAlignment((PySide6.QtCore.Qt.AlignCenter))
-                    new_line_edit.textChanged.connect(partial(self.validation ,row ,column))
-                    self.line_text[row][column] = new_line_edit
+        self.ui.dark_mode_toggle_btn.clicked.connect(partial(self.theme_toggle))
+        self.ui.exit_btn.clicked.connect(partial(self.Exit))
 
-          self.new_game()
+        self.ui.dark_mode_toggle_btn.setText("🌑")
+        self.line_text = [[None for row in range(9)] for column in range(9)]
 
-     def new_game(self):
-          puzzle = Sudoku(3, seed=randint(0, 1000)).difficulty(0.5)
-          for row in range(9):
-               for column in range(9):
-                    if puzzle.board[row][column] != None:
-                         self.line_text[row][column].setText(str(puzzle.board[row][column]))
-                         self.line_text[row][column].setReadOnly(True)
+        self.new_game()
+
+    def new_game(self):
+        self.puzzle = Sudoku(3, randint(0 ,1000)).difficulty(0.5)
+        for row in range(9):
+            for column in range(9):
+                new_line_edit = QLineEdit()
+                self.ui.grid_layout.addWidget(new_line_edit, row, column)
+                new_line_edit.setAlignment((PySide6.QtCore.Qt.AlignCenter))
+                new_line_edit.textChanged.connect(partial(self.validation ,row ,column))
+                self.line_text[row][column] = new_line_edit
+
+                if self.puzzle.board[row][column] != None:
+                    self.line_text[row][column].setText(str(self.puzzle.board[row][column]))
+                    self.line_text[row][column].setReadOnly(True)
+                else:
+                    self.line_text[row][column].setText("")
+
+    def solve(self):
+        solved_puzzle = Sudoku.solve(self.puzzle)
+        print(solved_puzzle)
+        self.msg_box.setWindowTitle("solve sudoku")
+        self.msg_box.setText(str(solved_puzzle))
+        self.msg_box.exec()
+
+    def about(self):
+        self.msg_box.setWindowTitle("about")
+        self.msg_box.setText("")
+        self.msg_box.exec()
+
+    def help(self):
+        self.msg_box.setWindowTitle("help")
+        self.msg_box.setText("")
+        self.msg_box.exec()
+
+    def open_file(self):
+        file_path = QFileDialog.getOpenFileName(self, 'Open text file...')[0]
+
+        with open(file_path, "r") as text_file:
+            text_file = open(file_path, "r")
+            text = text_file.read()
+            rows = text.split("\n")
+            text_file_board = [[None for row in range(9)] for column in range(9)]
+            for row in range(len(rows)):
+                cells = rows[row].split(' ')
+                for column in range(len(cells)):
+                    text_file_board[row][column] = int(cells[column])
+
+            for row in range(9):
+                for column in range(9):
+                    if text_file_board[row][column] != 0:
+                        self.line_text[row][column].setText(str(text_file_board[row][column]))
+                        self.line_text[row][column].setReadOnly(True)
                     else:
-                         self.line_text[row][column].setText("")
+                        self.line_text[row][column].setText("")
+                        self.line_text[row][column].setReadOnly(False)
 
-     def theme_toggle(self):
-          if self.mode == 0:
-               self.ui.dark_mode_toggle_btn.setText("🌕")
-               self.setStyleSheet("background-color: #404040; color: white;")
-               self.mode = 1
-          elif self.mode == 1:
-               self.ui.dark_mode_toggle_btn.setText("🌑")
-               self.setStyleSheet("")
-               self.mode = 0
+    def theme_toggle(self):
+        if self.mode == 0:
+            self.ui.dark_mode_toggle_btn.setText("🌕")
+            self.setStyleSheet("background-color: #404040; color: white;")
+            self.mode = 1
+        elif self.mode == 1:
+            self.ui.dark_mode_toggle_btn.setText("🌑")
+            self.setStyleSheet("")
+            self.mode = 0
 
-     def open_file(self):
-          file_path = QFileDialog.getOpenFileName(self, 'Open file...')[0]
+    def validation(self, row, column ,text):
+         if text not in ["1" ,"2" ,"3" ,"4" ,"5" ,"6" ,"7" ,"8" ,"9"]:
+              self.line_text[row][column].setText("")
 
-          with open(file_path, 'r') as text_file:
-               text_file = open(file_path, 'r')
-               text = text_file.read()
-               rows = text.split('\n')
-               puzzle_board = [[None for i in range(9)] for j in range(9)]
-               for i in range(len(rows)):
-                    cells = rows[i].split(' ')
-                    for j in range(len(cells)):
-                         puzzle_board[i][j] = int(cells[j])
+         self.check(self.line_text)
 
-               self.checkable = False
-               for i in range(9):
-                    for j in range(9):
-                         if puzzle_board[i][j] != 0:
-                              self.line_text[i][j].setText(str(puzzle_board[i][j]))
-                              self.line_text[i][j].setReadOnly(True)
-                         else:
-                              self.line_text[i][j].setText('')
-                              self.line_text[i][j].setReadOnly(False)
-               self.checkable = True
+    def check(self ,board):
+        for row in range(9):
+            for i in range(9):
+                for j in range(9):
+                    if self.line_text[row][i].text() == self.line_text[row][j].text() and i != j and self.line_text[row][i].text() != '':
+                        self.line_text[row][i].setStyleSheet("color: red;")
 
-     def validation(self, row, column ,text):
-          if text not in ["1" ,"2" ,"3" ,"4" ,"5" ,"6" ,"7" ,"8" ,"9"]:
-               self.line_text[row][column].setText("")
+        for col in range(9):
+            for i in range(9):
+                for j in range(9):
+                    if self.line_text[i][col].text() == self.line_text[j][col].text() and i != j and self.line_text[i][col].text() != '':
+                        self.line_text[i][col].setStyleSheet("color: red;")
 
-          self.check(self.line_text)
+        for row_start in range(0 ,9 ,3):
+            for column_start in range(0 ,9 ,3):
+                number_in_box = set()
+                for row in range(row_start, row_start+3):
+                    for col in range(column_start, column_start+3):
+                        number = board[row][col]
+                        if number in number_in_box:
+                            self.line_text[row][col].setStyleSheet("color:red;")
+                            return False
+                        number_in_box.add(number)
+        return True
 
-     def check(self ,board):
-          for row in board:
-               number_in_board = set()
-               for number in row:
-                    if number in number_in_board:
-                         self.line_text[row][number].setStyleSheet("color:red;")
-                         return False
-                    number_in_board.add(number)
-          
-          for column in range(9):
-               number_in_board = set()
-               for row in board:
-                    if row[column] in number_in_board:
-                         self.line_text[number][column].setStyleSheet("color:red;")
-                         return False
-                    number_in_board.add(number)
+    def check_winner(self):
+        solved_puzzle = Sudoku.solve(self.puzzle)
+        print(solved_puzzle)
+        for i in range(len(self.puzzle)):
+            ...
 
-          for row_start in range(0 ,9 ,3):
-               for column_start in range(0 ,9 ,3):
-                    number_in_box = set()
-                    for row in range(row_start, row_start+3):
-                         for col in range(column_start, column_start+3):
-                              number = board[row][col]
-                              if number in number_in_box:
-                                   self.line_text[row][column].setStyleSheet("color:red;")
-                                   return False
-                              number_in_box.add(number)
-          return True
+    def Exit(self):
+       	exit(0)
 
 if __name__ == "__main__":
-     app = QApplication(sys.argv)
-     window = Sudoku_game()
+    app = QApplication(sys.argv)
+    window = Sudoku_game()
 
-     window.show()
-     app.exec()
+    window.show()
+    app.exec()
