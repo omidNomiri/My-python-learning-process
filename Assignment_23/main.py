87d08bbc89a4c42f98c99c06068177d2c67ab206
omidNomiri <omidnomiri@gmail.com>
2024-02-03 10:25:05 +0330

    fix : sudoku is work

--- Assignment_23/main.py
+++ Assignment_23/main.py
@@ -3,10 +3,10 @@ from random import randint
 from sudoku import Sudoku
 from functools import partial
 import PySide6.QtCore
-from PySide6.QtWidgets import QMainWindow ,QApplication ,QLineEdit ,QFileDialog ,QMessageBox ,QSizePolicy
+from PySide6.QtWidgets import QMainWindow, QApplication, QLineEdit, QFileDialog, QMessageBox, QSizePolicy
 from ui_main_window import Ui_MainWindow
 
-class Sudoku_game(QMainWindow):
+class SudokuGame(QMainWindow):
     def __init__(self):
         super().__init__()
         self.ui = Ui_MainWindow()
@@ -14,7 +14,7 @@ class Sudoku_game(QMainWindow):
         self.ui.setupUi(self)
         self.mode = 0
         self.win = True
-        self.puzzle = Sudoku(3, randint(0 ,1000)).difficulty(0.5)
+        self.puzzle = Sudoku(3, seed=randint(0, 1000)).difficulty(0.5)
 
         self.ui.new_game_menu.triggered.connect(self.new_game)
         self.ui.open_file_menu.triggered.connect(self.open_file)
@@ -25,50 +25,48 @@ class Sudoku_game(QMainWindow):
         self.ui.dark_mode_toggle_btn.clicked.connect(partial(self.theme_toggle))
         self.ui.exit_btn.clicked.connect(partial(self.Exit))
 
-        self.ui.dark_mode_toggle_btn.setText("🌑")
+        self.ui.dark_mode_toggle_btn.setText("")
         self.line_text = [[None for row in range(9)] for column in range(9)]
 
         self.new_game()
 
     def new_game(self):
-        self.puzzle = Sudoku(3, randint(0 ,1000)).difficulty(0.5)
+        self.puzzle = Sudoku(3, seed=randint(0, 1000)).difficulty(0.5)
         for row in range(9):
             for column in range(9):
                 new_line_edit = QLineEdit()
                 self.ui.grid_layout.addWidget(new_line_edit, row, column)
                 new_line_edit.setAlignment((PySide6.QtCore.Qt.AlignCenter))
-                new_line_edit.setSizePolicy(QSizePolicy.Preferred , QSizePolicy.Preferred)
-                new_line_edit.textChanged.connect(partial(self.validation ,row ,column))
+                new_line_edit.setSizePolicy(QSizePolicy.Preferred, QSizePolicy.Preferred)
+                new_line_edit.textChanged.connect(partial(self.validation, row, column))
                 self.line_text[row][column] = new_line_edit
 
-                if self.puzzle.board[row][column] != None:
-                    self.line_text[row][column].setText(str(self.puzzle.board[row][column]))
-                    self.line_text[row][column].setReadOnly(True)
+                if self.puzzle.board[row][column] is not None:
+                    new_line_edit.setText(str(self.puzzle.board[row][column]))
+                    new_line_edit.setReadOnly(True)
                 else:
-                    self.line_text[row][column].setText("")
+                    new_line_edit.setText("")
 
     def solve(self):
         solved_puzzle = Sudoku.solve(self.puzzle)
-        print(solved_puzzle)
-        self.msg_box.setWindowTitle("solve sudoku")
+        self.msg_box.setWindowTitle("Solve Sudoku")
         self.msg_box.setText(str(solved_puzzle))
         self.msg_box.exec()
 
     def about(self):
-        self.msg_box.setWindowTitle("about")
-        self.msg_box.setText("")
+        self.msg_box.setWindowTitle("About")
+        self.msg_box.setText("Sudoku is a logic-based combinatorial number-placement puzzle. In classic Sudoku, the objective is to fill a 9 × 9 grid with digits so that each column, each row, and each of the nine 3 × 3 subgrids that compose the grid (also called boxes, blocks, or regions) contains all of the digits from 1 to 9. The puzzle setter provides a partially completed grid, which for a well-posed puzzle has a single solution.")
         self.msg_box.exec()
 
     def help(self):
-        self.msg_box.setWindowTitle("help")
-        self.msg_box.setText("")
+        self.msg_box.setWindowTitle("Help")
+        self.msg_box.setText("Sudoku grid consists of 9x9 spaces.\n You can use only numbers from 1 to 9.\n Each 3×3 block can only contain numbers from 1 to 9.\n Each vertical column can only contain numbers from 1 to 9.\n Each horizontal row can only contain numbers from 1 to 9.\n Each number in the 3×3 block, vertical column or horizontal row can be used only once.\n The game is over when the whole Sudoku grid is correctly filled with numbers.")
         self.msg_box.exec()
 
     def open_file(self):
         file_path = QFileDialog.getOpenFileName(self, 'Open text file...')[0]
 
         with open(file_path, "r") as text_file:
-            text_file = open(file_path, "r")
             text = text_file.read()
             rows = text.split("\n")
             text_file_board = [[None for row in range(9)] for column in range(9)]
@@ -96,14 +94,14 @@ class Sudoku_game(QMainWindow):
             self.setStyleSheet("")
             self.mode = 0
 
-    def validation(self, row, column ,text):
-        if text not in ["1" ,"2" ,"3" ,"4" ,"5" ,"6" ,"7" ,"8" ,"9"]:
+    def validation(self, row, column, text):
+        if text not in ["1", "2", "3", "4", "5", "6", "7", "8", "9"]:
             self.line_text[row][column].setText("")
 
         self.check(self.line_text)
         self.check_winner()
 
-    def check(self ,board):
+    def check(self, board):
         for row in range(9):
             for i in range(9):
                 for j in range(9):
@@ -116,8 +114,8 @@ class Sudoku_game(QMainWindow):
                     if self.line_text[i][col].text() == self.line_text[j][col].text() and i != j and self.line_text[i][col].text() != '':
                         self.line_text[i][col].setStyleSheet("color: red;")
 
-        for row_start in range(0 ,9 ,3):
-            for column_start in range(0 ,9 ,3):
+        for row_start in range(0, 9, 3):
+            for column_start in range(0, 9, 3):
                 number_in_box = set()
                 for row in range(row_start, row_start+3):
                     for col in range(column_start, column_start+3):
@@ -130,22 +128,21 @@ class Sudoku_game(QMainWindow):
 
     def check_winner(self):
         solved_puzzle = Sudoku.solve(self.puzzle)
-        print(solved_puzzle)
-        for i in range(len(self.puzzle)):
-            for j in range(len(self.puzzle)):
+        for i in range(len(self.line_text)):
+            for j in range(len(self.line_text)):
                 if solved_puzzle[i][j] != self.puzzle[i][j]:
                     self.win = False
         if self.win == True:
-            self.msg_box.setWindowTitle("win")
-            self.msg_box.setText("you win!")
+            self.msg_box.setWindowTitle("Win")
+            self.msg_box.setText("You win!")
             self.msg_box.exec()
 
     def Exit(self):
-       	exit(0)
+        exit(0)
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    window = Sudoku_game()
+    window = SudokuGame()
 
     window.show()
     app.exec()
