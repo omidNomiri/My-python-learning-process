30e7e579cdb793bf7bc604d539cbd53eecdea354
omidNomiri <omidnomiri@gmail.com>
2024-02-06 22:49:38 +0330

    feat : multiple file can convert

--- Assignment_24/video_to_audio.py
+++ Assignment_24/video_to_audio.py
@@ -3,9 +3,10 @@ from time import time
 
 start_time = time()
 
-video = editor.VideoFileClip("your mp4 file path")
-output = "your output mp3 file format"
-video.audio.write_audiofile(str(output))
+for i in range(1 ,4):
+	video = editor.VideoFileClip(f"Assignment_24\input\music_{str(i)}.mp4")
+	output = f"music_{str(i)}.mp3"
+	video.audio.write_audiofile(str(output))
 
 end_time = time()
 print(end_time - start_time)
