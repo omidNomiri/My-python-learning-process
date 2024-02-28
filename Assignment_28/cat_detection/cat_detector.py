c309c421ed426fe5d33b38bcde9f6ebbedc13435
omidNomiri <omidnomiri@gmail.com>
2024-02-28 21:11:13 +0330

    init : cat detector can show how many cat in image

---
+++ Assignment_28/cat_detection/cat_detector.py
@@ -0,0 +1,16 @@
+import cv2
+
+cat_image = cv2.imread("Assignment_28/cat_detection/cat_image.png")
+number_of_cat = 0
+cat_detector = cv2.CascadeClassifier(cv2.data.haarcascades +
+                                     "haarcascade_frontalcatface.xml")
+
+cats = cat_detector.detectMultiScale(cat_image, 1.3)
+for cat in cats:
+    number_of_cat += 1
+    x, y, w, h = cat
+    cv2.rectangle(cat_image, [x, y], [x+w, y+h], (255, 255), 2)
+
+print(f"number of cat: {number_of_cat}")
+cv2.imshow("", cat_image)
+cv2.waitKey(0)
