ec3a0a4504d327782b8e433163131eb6a572908c
omidNomiri <omidnomiri@gmail.com>
2024-03-22 15:17:15 +0330

    feat : select lip index

--- Assignment_30/fruit_filter.py
+++ Assignment_30/fruit_filter.py
@@ -27,10 +27,6 @@ start_time = time.perf_counter()
 boxes, scores = fd.inference(image)
 
 for pred in fa.get_landmarks(image, boxes):
-    # for index, p in enumerate(np.round(pred).astype(np.int16)):
-    #     cv2.circle(image, tuple(p), 4, color, -1)
-    #     cv2.putText(image, str(index), tuple(p), cv2.FONT_HERSHEY_PLAIN, 1, (0, 0, 0))
-
     lips_landmark = []
     for landmark in [52, 64, 63, 71, 67, 68, 61, 58, 59, 53, 56, 55]:
         lips_landmark.append(pred[landmark])
@@ -40,4 +36,3 @@ for pred in fa.get_landmarks(image, boxes):
 print(time.perf_counter() - start_time)
 cv2.imshow("result", result)
 cv2.waitKey(0)
-# cv2.imwrite("Assignment_30/result.png", image)
