14a18cfb02571481c90d89d7b6367d8a8d5e2f64
omidNomiri <omidnomiri@gmail.com>
2024-04-01 18:34:57 +0330

    fix : side edge detector noise redacted

--- Assignment_31/side_edge_detection.py
+++ Assignment_31/side_edge_detection.py
@@ -1,26 +1,28 @@
 import cv2
 import numpy as np
 
-image = cv2.imread("Assignment_31/input/house.png", cv2.IMREAD_GRAYSCALE)
+image = cv2.imread("Assignment_31/input/house.jpg", cv2.IMREAD_GRAYSCALE)
+
 
 def edge_detector(image, filter):
     row, column = image.shape
-    result = np.zeros((row, column), dtype=np.uint8)
+    result = np.zeros(image.shape, dtype=np.uint8)
 
     for i in range(1, row - 1):
         for j in range(1, column - 1):
             small_selection = image[i-1:i+2, j-1:j+2]
             result[i, j] = np.abs(np.sum(filter * small_selection))
-    
+
     return result
 
-horizontal_filter = np.array ([[-1 , -1 , -1] ,
-                               [0 , 0 , 0] ,
-                               [1 , 1 , 1]])
 
-vertical_filter = np .array ([[1 , 0 , -1] , 
-                              [1 , 0 , -1] , 
-                              [1 , 0 , -1]])
+horizontal_filter = np.array([[-.5, -.5, -.5],
+                              [0, 0, 0],
+                              [.5, .5, .5]])
+
+vertical_filter = np .array([[.5, 0, -.5],
+                             [.5, 0, -.5],
+                             [.5, 0, -.5]])
 
 result = edge_detector(image, horizontal_filter)
 cv2.imshow("", result)
